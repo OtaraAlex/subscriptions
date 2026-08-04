fbc558b754d66740ee8dad5eecf9a25d2f1e87ed
OtaraAlex <mbechealex61@gmail.com>
2026-08-04 03:04:33 +0300

    add routing & navigation

---
+++ app/(tabs)/subscriptions/[id].tsx
@@ -0,0 +1,15 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { Link, useLocalSearchParams } from "expo-router";
+
+const SubscriptionDetails = () => {
+  const { id } = useLocalSearchParams<{ id: string }>();
+  return (
+    <View>
+      <Text>Subscription Details: {id} </Text>
+      <Link href={{ pathname: "/subscriptions" }}>Back to Subscriptions</Link>
+    </View>
+  );
+};
+
+export default SubscriptionDetails;
